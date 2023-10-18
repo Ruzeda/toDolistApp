298d2e5ce399278469ddfebe27284926345069c2
Ruzeda <fields.rozay@gmail.com>
2023-10-18 13:40:06 -0700

    SummaryItem

---
+++ src/components/Summary/Summary.tsx
@@ -0,0 +1,18 @@
+const summaryItem = ({
+    itemName,
+    itemValue,
+}: {
+    itemName: string;
+    itemValue: number;
+}) => {
+    return (
+       <article className='bg-green-50 w-36 rounded-sm flex justify-between p-2'>
+        <h3 className="font-bold">{itemName}</h3>
+        <span className='g-green-900 text-white px-2 rounded-sm'>
+            {itemValue}
+        </span>
+       </article>
+    );
+};
+
+export default summaryItem;
