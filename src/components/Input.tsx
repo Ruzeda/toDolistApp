cbec53b43639b1334574c1596fd39507d4880fd7
Ruzeda <fields.rozay@gmail.com>
2023-10-18 14:35:48 -0700

    input and state added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 const InputContainer = () => {
+    const [newTask, setNewTask] = useState("");
     return (
         <form action="" className="flex cflex-col gap-4">
             <div className='flex flex-col'>
                 <label className="text-white">Enter your next task:</label>
-                <input className="p-1 rounded-sm" />
+                <input className="p-1 rounded-sm" 
+                    type='text'
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}    
+                />
             </div>
             <button
-                type='button'
+                type='submit'
                 className="bg-green-100 rounded-lg hover:bg-green-200 p-1"
             >
             Add Task
