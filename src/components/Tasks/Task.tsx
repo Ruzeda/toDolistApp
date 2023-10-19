6ac79e941c2b2b0cf165171b04954c23b0df8f91
Ruzeda <fields.rozay@gmail.com>
2023-10-19 00:10:16 -0700

    passed props to TaskItem

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,14 +1,33 @@
 import { Task } from "../../App";
 import TaskItem from "./TaskItem";
 
-const Task = ({ tasks }: { tasks: Task[] }) => {
+const Tasks = ({
+    tasks,
+    toggleDone,
+    handleDelete,
+  }: {
+    tasks: Task[];
+    toggleDone: (id: string, done: boolean) => void;
+    handleDelete: (id: string) => void;
+  }) => {
     return (
         <div className="flex flex-col gap-2">
-             {tasks.map((t) => (
-             <TaskItem key={t.id} name={t.name} />
-            ))}
+          {tasks.length ? (
+            tasks.map((t) => (
+              <TaskItem
+                key={t.id}
+                name={t.name}
+                done={t.done}
+                id={t.id}
+                toggleDone={toggleDone}
+                handleDelete={handleDelete}
+              />
+            ))
+          ) : (
+            <span className="text-green-100">No tasks yet!</span>
+          )}
         </div>
-    );
-};
-
-export default Task;
+      );
+    };
+    
+    export default Tasks;
